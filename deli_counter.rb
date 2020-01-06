88f81a90f301ecd21991f74c5a5e36c237f16b6c
Duncan Uruchima <duruchima1@gmail.com>
2020-01-06 01:11:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,9 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
   return place
 end
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else puts "Now serving #{katz_deli.first}"
+    katz_deli.shift
+  end

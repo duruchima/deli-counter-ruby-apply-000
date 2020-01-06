5b3abe91cbbb73fbd01067cae7d99c55718da535
Duncan Uruchima <duruchima1@gmail.com>
2020-01-06 00:43:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,12 @@
 katz_deli=[]
 def line(katz_deli)
   if katz_deli.length=0
-    puts "The line is currently empty."
-end
+    puts "The line is currently empty."    
+  else phrase = "The line is currently:"
+      
+    array.each_with_index do |value, index| 
+      phrase += " #{index.to_i+1}. #{value}"
+    end 
+    puts "#{message}"
+    end 
+  end 

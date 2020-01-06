a7c1ff129688178c3bc1dd004d3f57b0fb730850
Duncan Uruchima <duruchima1@gmail.com>
2020-01-06 01:14:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length==0
     puts "There is nobody waiting to be served!"
-  else puts "Now serving #{katz_deli.first}"
+  else puts "Currently serving #{katz_deli.first}"
     katz_deli.shift
   end
 end
